cfe863455bee7a5baa48ecc0d7b7d74ccad1f1aa
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-07-16 13:08:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Person(name,age){
     this.__proto__ = Person.prototype
 }
 Person.prototype.greet = function(){
-    console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`);
+    console.log(`Hello, my name is ${this._name} and I am ${this._age} years old.`);
 }
 function Employee(name,age,jobTitle){
     Person.call(this,name,age)
